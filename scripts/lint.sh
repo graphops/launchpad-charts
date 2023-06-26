17bc1ebf9348473a09c6576261b12d556ef14e97
cjorge-graphops <128147788+cjorge-graphops@users.noreply.github.com>
2023-06-26 20:17:36 +0100

    chore: small fixes on lint.sh script (#74)

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -1,26 +1,37 @@
 #!/bin/sh
 
 # We want to continue in the case of command failure, so let's not set this:
-# set -eo pipefail
+set -euo pipefail
 
 usage() {
-  echo "$(basename $0) <chart>"
+  echo "$(basename -- "$0") <chart>"
   echo "<chart> The name of chart in charts/ that you want to lint"
+  echo "Requires: container engine, kubeval, kube-score and helm available"
   exit 1
 }
 
-CHART_FOLDER=$1; shift
+crun() {
+if type podman > /dev/null; then
+  podman "$@"
+elif type docker > /dev/null; then
+  docker "$@"
+else
+  : "$@"
+fi
+}
 
-if [ -z "$CHART_FOLDER" ]; then
+CHART_FOLDER=${1:-}; shift || :
+
+if [ -z "$CHART_FOLDER" ] || ! type helm > /dev/null; then
   usage
 fi
 
-CHART_PATH="$(dirname $0)/../charts/$CHART_FOLDER"
+CHART_PATH="$(dirname -- "$0")/../charts/$CHART_FOLDER"
 
-TEMPLATE_OUTPUT=$(helm template $CHART_PATH 2>&1)
+TEMPLATE_OUTPUT="$(helm template "$CHART_PATH")"
 
 echo "$TEMPLATE_OUTPUT"
 
-echo "$TEMPLATE_OUTPUT" | kubeval
-echo "$TEMPLATE_OUTPUT" | kube-score score -
-echo "$TEMPLATE_OUTPUT" | docker run -i kubesec/kubesec scan /dev/stdin
+echo "$TEMPLATE_OUTPUT" | kubeval || :
+echo "$TEMPLATE_OUTPUT" | kube-score score - || :
+echo "$TEMPLATE_OUTPUT" | crun run -i kubesec/kubesec scan /dev/stdin || :
